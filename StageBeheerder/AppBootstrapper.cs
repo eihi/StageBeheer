1874845a632f99197915c58a31cc31148121d495
Esté Tigele <eihiara@gmail.com>
2013-06-19 14:30:08 +0200

    Folderstructuur

---
+++ StageBeheerder/AppBootstrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageBeheerder
+{
+    //public class AppBootstrapper : Bootstrapper
+    //{
+    //    private WindowManager windowManager;
+    //    private ViewController viewController;
+    //    private DefaultFactory defaultFactory;
+    //    private EntityService entityService;
+    //    private stagemanagerEntities smEntities;
+
+    //    public AppBootstrapper()
+    //        : base()
+    //    {
+    //        windowManager = new WindowManager();
+    //        defaultFactory = new DefaultFactory();
+    //        smEntities = new stagemanagerEntities();
+
+    //        entityService = new EntityService(defaultFactory, smEntities);
+    //        viewController = new ViewController();
+
+    //        // Observer observable
+    //        MainViewModel mainViewModel = new MainViewModel();
+    //        mainViewModel.SomethingHappened += viewController.HandleEvent;
+
+    //        // Show Window
+    //        windowManager.ShowWindow(mainViewModel);
+    //    }
+    //}
+}
