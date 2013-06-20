a538e953fefc145525716f2b33ffdc1db3678e79
Leon van Tuijl <leonvtuyl@hotmail.com>
2013-06-20 10:33:49 +0200

    DistanceCalculator toegevoegd
    Heeft 1 methode, Voer 2 strings in en returnt een double met de
    kilometer afstand.
    als de postcode niet bestaat/herkent dan returnt die 0

---
+++ StageManager/StageManager/Services/DistanceCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageManager.Services
+{
+    public class DistanceCalculator
+    {
+
+        public DistanceCalculator()
+        {
+        }
+
+        /*
+         *Voer de postcode strings in, deze worden naar een website gestuurd en daar word de afstand berekend in kilometers.
+         *syntax: 1342AA aan elkaar
+         *Returnt een double met de kilometer waarde.
+         */
+        public double getDistance(string p1, string p2)
+        {
+            //TODO : error handling bij error returnt die nu null
+            using (WebClient client = new WebClient())
+            {
+                string first = p1;
+                string second = p2;
+                String downloadedString = client.DownloadString("http://www.geenenict.nl/afstand.php?zip1=" + first + "&zip2=" + second);
+                System.Diagnostics.Debug.WriteLine(downloadedString);
+                double value;
+                double.TryParse(downloadedString, out value);
+                return value;
+
+            }
+        }
+
+    }
+}
