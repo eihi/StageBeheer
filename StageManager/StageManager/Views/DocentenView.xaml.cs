045fbbb876a7d62a96d6db597d78e7a63cfc06b3
nope <escapedinabookchest@gmail.com>
2013-06-20 23:05:37 +0200

    Views

---
+++ StageManager/StageManager/Views/DocentenView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace StageManager.Views
+{
+    /// <summary>
+    /// Interaction logic for DocentenView.xaml
+    /// </summary>
+    public partial class DocentenView : UserControl
+    {
+        public DocentenView()
+        {
+            InitializeComponent();
+        }
+    }
+}
