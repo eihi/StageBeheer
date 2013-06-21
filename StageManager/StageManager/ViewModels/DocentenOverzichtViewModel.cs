4749666d4ec17095130fa9dfd6aaaf5179b29dce
Esté Tigele <eihiara@gmail.com>
2013-06-21 14:54:27 +0200

    Overzichten

---
+++ StageManager/StageManager/ViewModels/DocentenOverzichtViewModel.cs
@@ -0,0 +1,19 @@
+﻿using StageManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageManager.ViewModels
+{
+    class DocentenOverzichtViewModel : PropertyChanged
+    {
+
+        public DocentenOverzichtViewModel(MainViewModel main)
+            : base(main)
+        {
+        }
+
+    }
+}
---
+++ StageManager/StageManager/ViewModels/StudentenOverzichtViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using StageManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageManager.ViewModels
+{
+    class StudentenOverzichtViewModel : PropertyChanged
+    {
+        public StudentenOverzichtViewModel(MainViewModel main)
+            : base(main)
+        {
+        }
+
+        //public studentenoverzichtviewmodel(mainviewmodel main, students student)
+        //    : this(main)
+        //{
+        //    // todo
+        //}
+    }
+}
